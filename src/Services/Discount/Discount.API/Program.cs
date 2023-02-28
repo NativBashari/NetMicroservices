590ff9b2906a8423ae8923720114051d66a49165
NativBashari <nativbas2000@gmail.com>
2023-02-28 15:30:45 +0200

    Discount.API microservice developed

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,10 +1,22 @@
+using Discount.API.Extensions;
+using Discount.API.Repositories;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var host = Host.CreateDefaultBuilder(args).Build();
+host.MigrateDatabase<Program>();
+//host.Run();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Discount.API", Version = "v1" });
+});
+builder.Services.AddControllers();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,7 +25,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseRouting();
+app.UseEndpoints(endpoint =>
+{
+    endpoint.MapControllers();
+});
 
 
 app.Run();
