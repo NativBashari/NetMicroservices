c861e1b7c19e03230fc74269bf915b62324b6655
NativBashari <nativbas2000@gmail.com>
2023-03-02 17:25:15 +0200

    Get order list by userName added

---
+++ src/Services/Ordering/Ordering.Application/Models/EmailSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Models
+{
+    internal class EmailSettings
+    {
+        public string ApiKey { get; set; } = string.Empty;
+        public string FromAdress { get; set; } = string.Empty;
+        public string FromName { get; set; } = string.Empty;
+    }
+}
